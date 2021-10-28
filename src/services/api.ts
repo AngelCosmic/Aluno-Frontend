82e03581ada92dc695d1937bef1cf893f30f6646
AngelCosmic <ogecrom12@gmail.com>
2021-10-28 10:27:31 -0300

    Adicionando variáveis de ambiente

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,10 @@
-import axios from 'axios'
+import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
  
 const api = axios.create({
-    baseURL: 'https://aluno-backend1.herokuapp.com'
+    baseURL: process.env.REACT_APP_BACKEND
 })
  
 export default api;
